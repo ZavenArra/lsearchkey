f17e9d1fd6466b97e6b80b540f08cf37c7a1fee3
Deepwinter <deepwinter@winterroot.net>
2013-05-18 17:35:23 -0700

    Implemented distinct key return

--- classes/lkeysearch.php
+++ classes/lkeysearch.php
@@ -12,7 +12,6 @@ class lkeysearch {
 			$inserts[] = "( '$tag', '$keys[$i]', $record_ids[$i] )";
 		}
 		$insert_sql = "INSERT INTO lsearchkeys ( tag, search_key, record_id ) VALUES ".implode($inserts, ",");
-		Kohana::$log->add(Log::INFO, $insert_sql);
 
 		$db = Database::instance();
 		$db->query(Database::INSERT, $insert_sql);
@@ -24,7 +23,62 @@ class lkeysearch {
 		$db->query(Database::DELETE, $sql);
 	}
 
-	static function search_keys_like($search, $offset, $limit){
+	static function distinct_search_keys_with_tag($tag, $sort = 'ASC'){
+		
+		$results = DB::select('search_key')
+			->from('lsearchkeys')
+			->distinct(true)
+			->where('tag', '=', $tag)
+			->order_by('search_key', $sort)
+			->execute();
+		return $results;
+	}
+
+	static function search_keys_like($search, $offset, $limit, $tag, $sort = 'ASC'){
+		$results = ORM::Factory('lsearchkey')
+		->where('tag', '=', $tag)
+		->and_where_open()
+		->where('search_key', 'LIKE', "$search%")
+		->or_where('search_key', 'LIKE', "% $search%")
+		->and_where_close()
+		->order_by('search_key', $sort)
+		->offset($offset)
+		->limit($limit)
+		->find_all();
+		return $results;
+	}
+
+	static function search_keys_like_distinct($search, $offset, $limit, $tag){
+		$sql =  "SELECT DISTINCT record_id, min(search_key) as min_search_key ".
+			"FROM lsearchkeys ".
+			"WHERE tag = '$tag' ".
+			"AND ( search_key LIKE '$search%' OR search_key LIKE '% $search%' ) ".
+			"GROUP BY record_id ".
+			"ORDER BY min_search_key ".
+			"LIMIT $offset, $limit";
+		Kohana::$log->add(Log::INFO, $sql);
+		$db = Database::instance();
+		$results = $db->query(Database::SELECT, $sql);
+		return $results;
+	}
+
+
+	static function joined_records_like($search, $offset, $limit, $tag, $table){
+		$sql = "SELECT * FROM $table ".
+			"JOIN  ( ".
+			"SELECT DISTINCT record_id, min(search_key) as min_search_key ".
+			"FROM lsearchkeys ".
+			"WHERE tag = '$tag' ".
+			"AND ( search_key LIKE '$search%' OR search_key LIKE '% $search%' ) ".
+			"GROUP BY id ".
+			"ORDER BY min_search_key ".
+			" ) search_results ON $table.id = search_results.record_id ".
+			" LIMIT $offset, $limit ".
+			" order_by search_results.min_search_key ";
+						
+		$db = Database::instance();
+		$results = $db->query(Database::SELECT, $sql);
+		return $results;
 
 	}
 
--- tests/classes/LKeySearchTest.php
+++ tests/classes/LKeySearchTest.php
@@ -1,15 +1,18 @@
 <?
 Class LKeySearchTest extends Kohana_UnitTest_TestCase {
-	public static $keys = array('a', 'ab', 'abc', 'b', 'before', 'd', 'de', 'da', 'dad', 'z');
+	public static $keys = array('a', 'ab', 'abc', 'b', 'before', 'd', 'd', 'da', 'dad', 'a');
 	public static $record_ids = array(1, 10, 12, 3, 4, 345, 233, 455, 34, 2);
 
-  public static function setUpBeforeClass(){
-  }
+	public static $keys_many_to_many = array('a', 'ab', 'abc', 'b', 'before', 'd', 'd', 'da', 'dad', 'a');
+	public static $record_ids_many_to_many = array(1, 1, 1, 2, 2, 2, 3, 455, 3, 3);
 
-  public static function tearDownAfterClass(){
+  public static function setUpBeforeClass(){
 		$sql = "DELETE FROM lsearchkeys";
 		$db = Database::instance();
 		$db->query(Database::DELETE, $sql);
+  }
+
+  public static function tearDownAfterClass(){
   }
 
   public function testAddKeysFunctionExists(){
@@ -24,7 +27,7 @@ Class LKeySearchTest extends Kohana_UnitTest_TestCase {
   public function testAddKeys(){
 		lkeysearch::add_keys_with_tag( 'testKeys', self::$keys, self::$record_ids );
 		$all = ORM::factory('lsearchkey')->where('tag', '=', 'testKeys')->find_all();
-		$this->assertEquals(count($all), count(self::$keys));
+		$this->assertEquals(count(self::$keys), count($all) );
   }
 
   /**
@@ -33,84 +36,38 @@ Class LKeySearchTest extends Kohana_UnitTest_TestCase {
   public function testDropKeys(){
 		lkeysearch::drop_keys_with_tag( 'testKeys' );
 		$all = ORM::factory('lsearchkey')->find_all();
-		$this->assertEquals(count($all), 0);
+		$this->assertEquals(0, count($all));
   }
 
+	public function testDistinctKeys(){
+		$tag = 'distinctTest';
+		lkeysearch::add_keys_with_tag( $tag, self::$keys, self::$record_ids );
+		$results = lkeysearch::distinct_search_keys_with_tag($tag, $sort = 'ASC');
+		lkeysearch::drop_keys_with_tag( $tag );
+		$this->assertEquals(8, count($results));
+	}
 
-	/*
-  public function testAssociatorPoolWithAll(){
+	public function testSearchLike(){
+		$tag = 'likeTest';
+		lkeysearch::add_keys_with_tag( $tag, self::$keys, self::$record_ids );
+		$r1 = lkeysearch::search_keys_like('a', 0, 100, $tag);
+		$r2 = lkeysearch::search_keys_like('ab', 0, 100, $tag);
+		//lkeysearch::drop_keys_with_tag( $tag);
 
-    $a = new Associator('single-association', 'myAssociation');
-    $this->assertNotNULL($a->pool);
-    $this->assertTrue(count($a->pool) > 0);
-    return $a;
-  }
-
-  public function testAssociatorPoolWithObjectTypeName(){
-
-    $a = new Associator('single-association', 'myAssociation', $this->articleFilter);
-    $this->assertNotNULL($a->pool);
-    $this->assertTrue(count($a->pool) > 0);
-    return $a;
-  }
-
-  /**
-   * @depends testNewAssociator
-   *o/*/
-	/*
-  public function testAssociatorRender($a){
-    $html = $a->render();
-    $this->assertNotNull($html);
-  }
-
-  /**
-   * @depends testNewAssociator
-   */
-	/*
-  public function testAssociatorPoolRender($a){
-    $html = $a->renderPoolItems();
-    $this->assertNotNull($html);
-  }
-
-  /**
-   * @depends testNewAssociatorWithObjectTypeName
-   */
-	/*
-  public function testAssociatorRenderWithObjectTypeFilter($a){
-    $html = $a->render();
-    $this->assertNotNull($html);
-  }
+		$this->assertEquals(4, $r1->count());
+		$this->assertEquals(2, $r2->count());
+	}
 
 
+	public function testSearchLikeManyToMany(){
+		$tag = 'likeTestManyToMany';
+		lkeysearch::add_keys_with_tag( $tag, self::$keys_many_to_many, self::$record_ids_many_to_many );
+		$r1 = lkeysearch::search_keys_like_distinct('a', 0, 100, $tag);
+		$r2 = lkeysearch::search_keys_like_distinct('', 0, 100, $tag);
+		//lkeysearch::drop_keys_with_tag( $tag);
 
-
-  public function testAssociatorPoolExcludesAssociated(){
-    $object1 = Graph::create_object('article', 'test1');
-    $object1 = Graph::object($object1);
-    $object2 = Graph::create_object('article', 'test2');
-    $object2 = Graph::object($object2);
-    $object3 = Graph::create_object('article', 'test3');
-    $object3 = Graph::object($object3);
-
-    $object1->add_lattice_relationship('testAssociation', $object2->id);
-
-    $a = new Associator($object1->id, 'testAssociation', $this->articleFilter);
-    $this->assertTrue($this->resultContainsObjectWithId($a->associated, $object2->id));
-    $this->assertFalse($this->resultContainsObjectWithId($a->pool, $object2->id));
-
-    $object1->delete();
-    $object2->delete();
-    $object3->delete();
-  }
-
-  private function resultContainsObjectWithId($result, $id){
-    foreach($result as $row){
-      if ($row->id == $id){
-        return true;
-      }
-    }
-    return false;
-  }
-	 */
+		$this->assertEquals(2, count($r1));
+		$this->assertEquals(4, count($r2));
+	}
 
 }
