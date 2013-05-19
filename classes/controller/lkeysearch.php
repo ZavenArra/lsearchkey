1124f45bb1ecd377ed25c91d917ff05fcb00ad62
Deepwinter <deepwinter@winterroot.net>
2013-05-18 20:41:41 -0700

    Fixed typo

--- classes/controller/lkeysearch.php
+++ classes/controller/lkeysearch.php
@@ -6,7 +6,7 @@ Class Controller_LKeySearch extends Lattice_Controller_Ajax {
 		$results = lkeysearch::search_keys_like($search, $offset, $limit, $tag);
 		$keys = array();
 		foreach($results as $key){
-			$keys[] = $key->search_key;]	
+			$keys[] = $key->search_key;
 		}
 		$this->response->data(array('search_keys'=>$keys));
 	}
